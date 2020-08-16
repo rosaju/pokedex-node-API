284b4dbb95ca9c54f3d3930afdd0a96d5b8934b6
rosaju <rosajuuu@gmail.com>
2020-08-15 22:26:32 -0300

    adição do cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,6 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+const corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 app.use('/', indexRouter);
 app.use('/pokemon-infos', pokemonInfosRouter);
